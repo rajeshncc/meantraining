b15366765ca9e90e41d43661c33e68d51e1b4c3e
Kuppusamy <rkuppusamy@in.imshealth.com>
2021-08-04 12:40:52 +0530

    this is my first commit

---
+++ Day3-js/myjs.js
@@ -0,0 +1,11 @@
+console.log("this script from myjs.js");
+
+
+document.getElementById("h").style.color = 'red';
+
+
+function changeH()
+{
+    document.getElementById("h").innerHTML = "Welcome to Javascript";
+    document.getElementById("h").style.backgroundColor = 'blue';
+}
